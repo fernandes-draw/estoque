97ebccab0843f540656cea158be88fde5abbb4ca
Everson Fernandes <fernandes_draw@hotmail.com>
2021-11-26 14:40:36 -0300

    Ajustando o Admin. Close #7

--- projeto/estoque/admin.py
+++ projeto/estoque/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Estoque, EstoqueItens
+
+
+@admin.register(Estoque)
+class EstoqueAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "nf",
+    )
+    search_fields = ("nf",)
+    list_filter = ("funcionario",)
+    date_hierarchy = "created"
--- projeto/produto/admin.py
+++ projeto/produto/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Produto
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "importado",
+        "ncm",
+        "preco",
+        "estoque",
+        "estoque_minimo",
+    )
+    search_fields = ("produto",)
+    list_filter = ("importado",)
