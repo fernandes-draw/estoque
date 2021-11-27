69fa16f0199549f8e9edf2c5794a99441b5df021
Everson Fernandes <fernandes_draw@hotmail.com>
2021-11-27 14:20:44 -0300

    Estoque Entrada: estoque_entrada_list.html. Close #16

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Estoque
+
+
+def estoque_entrada_list(request):
+    template_name = "estoque_entrada_list.html"
+    objects = Estoque.objects.filter(movimento="e")
+    context = {"object_list": objects}
+    return render(request, template_name, context)
