1ce7a2a57ed87b29cba5fdb6defb455c2b097329
Everson Fernandes <fernandes_draw@hotmail.com>
2021-11-29 14:52:06 -0300

    Estoque entrada: dar entrada no estoque. Close #20

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -2,6 +2,7 @@ from django.forms.models import inlineformset_factory
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, resolve_url
 from projeto.estoque.forms import EstoqueForm, EstoqueItensForm
+from projeto.produto.models import Produto
 
 from .models import Estoque, EstoqueItens
 
@@ -18,6 +19,17 @@ def estoque_entrada_detail(request, pk):
     obj = Estoque.objects.get(pk=pk)
     context = {"object": obj}
     return render(request, template_name, context)
+
+
+def dar_baixa_estoque(form):
+    """Pega os produtos a partir da instância do formulário (Estoque)"""
+    produtos = form.estoques.all()
+    for item in produtos:
+        produto = Produto.objects.get(pk=item.produto.pk)
+        produto.estoque = item.saldo
+        produto.save()
+    print("Estoque atualizado com sucesso!")
+    return
 
 
 def estoque_entrada_add(request):
@@ -41,6 +53,7 @@ def estoque_entrada_add(request):
         if form.is_valid() and formset.is_valid():
             form = form.save()
             formset.save()
+            dar_baixa_estoque(form)
             url = "estoque:estoque_entrada_detail"
             return HttpResponseRedirect(resolve_url(url, form.pk))
     else:
