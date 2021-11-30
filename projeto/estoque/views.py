2bec051dd126878ed2621a6d9cbb50a6e7ed3bea
Everson Fernandes <fernandes_draw@hotmail.com>
2021-11-30 12:34:34 -0300

    Transformando estoque_entrada_detail em CBV. Close #32

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -1,7 +1,7 @@
 from django.forms.models import inlineformset_factory
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, resolve_url
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from projeto.estoque.forms import EstoqueForm, EstoqueItensForm
 from projeto.produto.models import Produto
 
@@ -38,6 +38,16 @@ def estoque_entrada_detail(request, pk):
         "url_list": "estoque:estoque_entrada_list",
     }
     return render(request, template_name, context)
+
+
+class EstoqueEntradaDetail(DetailView):
+    model = EstoqueEntrada
+    template_name = "estoque_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(EstoqueEntradaDetail, self).get_context_data(**kwargs)
+        context["url_list"] = "estoque:estoque_entrada_list"
+        return context
 
 
 def dar_baixa_estoque(form):
@@ -125,6 +135,16 @@ def estoque_saida_detail(request, pk):
         "url_list": "estoque:estoque_saida_list",
     }
     return render(request, template_name, context)
+
+
+class EstoqueSaidaDetail(DetailView):
+    model = EstoqueSaida
+    template_name = "estoque_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(EstoqueSaidaDetail, self).get_context_data(**kwargs)
+        context["url_list"] = "estoque:estoque_saida_list"
+        return context
 
 
 def estoque_saida_add(request):
