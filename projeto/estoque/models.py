c5e6ad5f0e78e6fe9ce5fdc96d749ad0426c9337
Everson Fernandes <fernandes_draw@hotmail.com>
2021-11-30 12:47:43 -0300

    Fazendo apenas um estoque_detail. Close #33

--- projeto/estoque/models.py
+++ projeto/estoque/models.py
@@ -38,9 +38,6 @@ class EstoqueEntrada(Estoque):
         verbose_name = "estoque entrada"
         verbose_name_plural = "estoque entrada"
 
-    def get_absolute_url(self):
-        return reverse_lazy("estoque:estoque_entrada_detail", kwargs={"pk": self.pk})
-
 
 class EstoqueSaida(Estoque):
     objects = EstoqueSaidaManager()
@@ -49,9 +46,6 @@ class EstoqueSaida(Estoque):
         proxy = True
         verbose_name = "estoque saida"
         verbose_name_plural = "estoque saida"
-
-    def get_absolute_url(self):
-        return reverse_lazy("estoque:estoque_saida_detail", kwargs={"pk": self.pk})
 
 
 class EstoqueItens(models.Model):
