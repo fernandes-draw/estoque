bfa6ee7bf67dfbc5459c85b8b80bed4aa890fe8e
Everson Fernandes <fernandes_draw@hotmail.com>
2021-11-30 15:09:30 -0300

    Fazendo Login. Close #35

--- projeto/settings.py
+++ projeto/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from typing import cast
 
@@ -129,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "/staticfiles/")
+
+LOGIN_URL = "/admin/login/"
+LOGOUT_REDIRECT_URL = "core:index"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
